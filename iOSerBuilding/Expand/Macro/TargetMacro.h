80b2a84d48ced0188711382e3e24350a96e814f6
Zhanghaichuan <924320752@qq.com>
2019-04-22 10:43:12 +0800

    删除多Target管理，改用宏管理开发环境。

---
+++ iOSerBuilding/Expand/Macro/TargetMacro.h
@@ -0,0 +1,14 @@
+//
+//  TargetMacro.h
+//  iOSerBuilding
+//
+//  Created by 张海川 on 2019/4/22.
+//
+
+#ifndef TargetMacro_h
+#define TargetMacro_h
+
+// 0测试 1正式 2预发
+#define APPSTATUS   2
+
+#endif /* TargetMacro_h */
